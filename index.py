fddcbfe5dea23effdd8eae0cd8573ca9d9dffbb0
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-07 15:49:45 -0500

    Update index.py

--- index.py
+++ index.py
@@ -21,6 +21,14 @@ title = html.Div(
     ], className='card-title'
 )
 
+collapse_content = dbc.Card([dbc.Row([            html.Div("One of three columns"),
+#     dbc.Col(time_interval_filter),
+#                                       dbc.Col(bundle_filter),
+#                                       dbc.Col(category_filter),
+#                                       dbc.Col(blockchain_filter)
+]
+                                     )], color="light")
+
 navbar = html.Div(
     [
         dbc.Button(
@@ -31,8 +39,7 @@ navbar = html.Div(
             n_clicks=0,
         ),
         dbc.Collapse(
-#             collapse_content,
-            html.Div("One of three columns"),
+            collapse_content,
             id="collapse",
             is_open=False,
         ),
