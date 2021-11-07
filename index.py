93334707feaeb52e6f3a448b9a068506a552abe0
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-07 15:41:26 -0500

    Update index.py

--- index.py
+++ index.py
@@ -20,38 +20,38 @@ title = html.Div(
     ], className='card-title'
 )
 
-navbar = html.Div(
-    [
-        dbc.Button(
-            "Filters",
-            id="collapse-button",
-            className="mb-3",
-            color="primary",
-            n_clicks=0,
-        ),
-        dbc.Collapse(
-#             collapse_content,
-            html.Div("One of three columns"),
-            id="collapse",
-            is_open=False,
-        ),
-    ],
-)
+# navbar = html.Div(
+#     [
+#         dbc.Button(
+#             "Filters",
+#             id="collapse-button",
+#             className="mb-3",
+#             color="primary",
+#             n_clicks=0,
+#         ),
+#         dbc.Collapse(
+# #             collapse_content,
+#             html.Div("One of three columns"),
+#             id="collapse",
+#             is_open=False,
+#         ),
+#     ],
+# )
 
 
-@app.callback(
-    Output("collapse", "is_open"),
-    [Input("collapse-button", "n_clicks")],
-    [State("collapse", "is_open")],
-)
-def toggle_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
+# @app.callback(
+#     Output("collapse", "is_open"),
+#     [Input("collapse-button", "n_clicks")],
+#     [State("collapse", "is_open")],
+# )
+# def toggle_collapse(n, is_open):
+#     if n:
+#         return not is_open
+#     return is_open
 
 
 app.layout = html.Div([title,
-                       navbar,
+#                        navbar,
 #                        cards,
 #                        attribution
                       ]
