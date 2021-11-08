8b8c64eea0c605735fdaa60eece1ef0631dd4395
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-07 20:10:56 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import os
 
 import dash
-# from dash import Input, Output, State, html
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
