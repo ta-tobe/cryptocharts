8e7744b5751ab08273584839be35a2793303d342
ta-tobe <53129289+ta-tobe@users.noreply.github.com>
2021-11-07 22:10:36 -0500

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash_bootstrap_components._components.Container import Container
+import pandas as pd
 
 
 
